12f4501523fd307bd75c7c9389c1ca70a0397d47
r-sierra <roberto@ideadigital.com.ar>
2021-06-15 19:23:05 -0300

    Add BooksAPI interaction to Search Component

--- src/Search.js
+++ src/Search.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 
 class Search extends Component {
 
   state = {
-    query: ''
+    query: '',
+    books: [],
   }
 
   handleInputChange = event => {
     const value = event.target.value
     this.setState({ query: value })
+
+    BooksAPI.search(value).then((books) => {
+      this.setState({ books })
+    })
   }
 
   render() {
-    const query = this.state.query
+    const { query, books } = this.state
 
     return (
       <div className="search-books">
@@ -22,14 +28,6 @@ class Search extends Component {
             <button className="close-search">Close</button>
           </Link>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
             <input
               type="text"
               value={query}
@@ -39,7 +37,9 @@ class Search extends Component {
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {books.map((book) => (<li key={book.id}>{book.title}</li>))}
+          </ol>
         </div>
       </div>
     )
