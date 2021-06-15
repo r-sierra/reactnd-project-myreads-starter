fe5202206738869300ed4da0337c338d79a7e16e
r-sierra <roberto@ideadigital.com.ar>
2021-06-15 20:03:06 -0300

    Create Book stateless functional component

---
+++ src/Book.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Book = props => {
+  const { title, authors, thumbnail } = props
+
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${thumbnail})` }}></div>
+        <div className="book-shelf-changer">
+          <select>
+            <option value="move" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
+      </div>
+      <div className="book-title">{ title }</div>
+      <div className="book-authors">{ authors.join(', ') }</div>
+    </div>
+  )
+}
+
+Book.propTypes = {
+  title: PropTypes.string.isRequired,
+  authors: PropTypes.array.isRequired,
+  thumbnail: PropTypes.string.isRequired,
+}
+
+export default Book
